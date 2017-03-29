86738f00412201ea492cfb8636e2a15197f9f392
Python Server <pyserver@kast76.utlib.ee>
2017-03-29 08:45:24 +0300

    json output

---
+++ app/lemmatizer.py
@@ -0,0 +1,36 @@
+from estnltk import Text
+from collections import Counter
+
+stopwords = ["aga", "ehk", "ei", "et", "iga", "ikka", "ise", "ja", "ju", "ka", "kas", \
+"keegi", "kes", "kui", "kuidas", "kõik", "mina", "mis", "miski", "nii", "olema", "oma", \
+"pidama", "saama", "sama", "seal", "see", "siis", "sina", "tema", "tulema", "vaid", "veel", \
+"võima", "välja", "ära", "üks", "üle", "ning", "ega", "nagu", "sest", "minema", "tulema", \
+"tegema", "teine"]
+
+def lemmatize(text):
+    """
+    Parameters
+    ----------
+    text : string
+        Text to lemmatize
+
+
+    Output
+    ------
+    return : string
+        Lemmatized text
+    """
+    words = []
+    for word in list(Text(text).get.lemmas.postags.as_zip): # get all lemmas of input words and and their POS-s
+        if "|" in word[0] and "V" in word[1]: # for dambigious verbs
+            for ambig in word[0].split("|"):
+                if ambig.endswith("ma"):
+                    words.append(ambig)
+                    break # because several lemmas can end with "ma", choose only the first one
+        elif "|" in word[0]: # all other ambigious words
+            words.append(word[0].split("|")[0])
+        elif "Z" not in word[1]: # exclude punctuation
+            words.append(word[0])
+    stopwords_removed = [w for w in words if w not in stopwords]
+    return Counter(stopwords_removed)
+
--- app/views.py
+++ app/views.py
@@ -1,55 +1,20 @@
 from app import app
-from math import cos
 from flask import request
 from flask import render_template
-from estnltk import Text
-
+from app import lemmatizer
+import json
+#from estonian_wordcloud import lemm
 
 @app.route("/")
-@app.route("/index")
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
     return render_template('index.html',
-                           title='Home',
-                           user=user)
-
-@app.route("/cos")
-def app_cos():
-    value = float(request.args.get("v"))
-    shift = float(request.args.get("shift"))
-    return "The cos of {} is {:.3f}".format(value, cos(value) + float(shift))
-
-
-def lemmatize(text):
-    """
-    Parameters
-    ----------
-    text : string
-        Text to lemmatize
-
-
-    Output
-    ------
-    return : string
-        Lemmatized text
-    """
-    words = []
-    for word in list(Text(text).get.lemmas.postags.as_zip):
-        if "|" in word[0] and "V" in word[1]:
-            for ambig in word[0].split("|"):
-                if ambig.endswith("ma"):
-                    words.append(ambig)
-                    break # sest ma-ga lõppevaid lemmasid võib samal sõnal olla mitu, võta ainult esimene
-        elif "|" in word[0]:
-            words.append(word[0].split("|")[0])
-        elif "Z" not in word[1]:
-            words.append(word[0])
-    return " ".join(words)
-
+                           title='Home')
 
 @app.route('/lemmatize_form/', methods=['POST'])
 def lemmatize_form():
     text = request.form['text']
-    print(text)
-    return render_template('lemmatized.html', lem_text=lemmatize(text))
+    #print(text)
+    lem_freq = lemmatizer.lemmatize(text)
+    #return render_template('lemmatized.html', lem_text=lemmatizer.lemmatize(text))
+    return json.dumps(lem_freq)
 
