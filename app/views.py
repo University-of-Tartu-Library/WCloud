c83e9e4ccc4d5ec34f904fc1f2339f4d34cc40e3
Maksim Misin <mishin1991@gmail.com>
2017-03-30 14:10:22 +0300

    jsonp

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import render_template
 from app import lemmatizer
 import json
+from functools import wraps
 #from estonian_wordcloud import lemm
 
 #@app.route("/")
@@ -10,6 +11,20 @@ import json
 #    return render_template('index.html',
 #                           title='Home')
 
+
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f(*args,**kwargs).data) + ')'
+            return current_app.response_class(content, mimetype='application/javascript')
+        else:
+            return f(*args, **kwargs)
+return decorated_function
+
+@support_jsonp
 @app.route('/create_bubbles', methods=['POST'])
 def create_bubbles():
     text = request.json['text']
